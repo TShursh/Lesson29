4b0d945be5d37c6f1111410bebb472533d2f1e6f
Bazylevich <victoria.bazylevich@gmail.com>
2022-12-08 21:29:39 +0300

    Initial commit

---
+++ prod/model/entity/__init__.py
@@ -0,0 +1,6 @@
+from prod.model.entity.product import Product
+from prod.model.entity.orange import Orange
+from prod.model.entity.milk import Milk
+from prod.model.entity.bread import Bread
+
+from prod.model.entity.basket import Basket
---
+++ prod/model/entity/product.py
@@ -0,0 +1,27 @@
+class Product:
+
+    def __init__(self, price=0):
+        self.price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, price):
+        if price >= 0:
+            self._price = price
+        else:
+            if not hasattr(self, "_price"):
+                self._price = 0
+
+    @price.deleter
+    def price(self):
+        del self._price
+
+    def __str__(self):
+        return f"price = {self._price}"
+
+# p = Product(22)
+# p.price = -20
+# print(p)
